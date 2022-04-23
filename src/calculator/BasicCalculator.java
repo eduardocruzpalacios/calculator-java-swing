5f7329ec2442bb52c82977a826df5ebf8238bc20
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-04-23 15:45:52 +0200

    reorganize packages and files

---
+++ src/calculator/BasicCalculator.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class BasicCalculator {
+
+	public BasicCalculator() {
+	}
+
+	public void launch() {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					initialize();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private JFrame frame;
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
--- src/control/Main.java
+++ src/control/Main.java
@@ -1,43 +1,12 @@
 package control;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import calculator.BasicCalculator;
 
 public class Main {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main window = new Main();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public Main() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		BasicCalculator basicCalculator = new BasicCalculator();
+		basicCalculator.launch();
 	}
 
 }
