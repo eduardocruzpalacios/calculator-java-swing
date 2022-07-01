9b8101cfb744fb5e8a1db05a5e20fe9bf9d3c9ed
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-07-01 20:15:03 +0200

    extract the piece of logic for loading the view to a method to keep initialize() method having same abstract level

--- src/calculator/BasicCalculator.java
+++ src/calculator/BasicCalculator.java
@@ -64,88 +64,7 @@ public class BasicCalculator {
 	 */
 	private void initialize() {
 
-		// VIEW
-
-		frame = new JFrame();
-		frame.setBounds(100, 100, 522, 622);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.getContentPane().setLayout(null);
-
-		textField = new JTextField();
-		textField.setText("");
-		textField.setEnabled(false);
-		textField.setBounds(49, 30, 391, 70);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-
-		btn1 = new JButton("1");
-		btn1.setBounds(49, 231, 56, 59);
-		frame.getContentPane().add(btn1);
-
-		btn2 = new JButton("2");
-		btn2.setBounds(115, 231, 56, 59);
-		frame.getContentPane().add(btn2);
-
-		btn3 = new JButton("3");
-		btn3.setBounds(181, 231, 56, 59);
-		frame.getContentPane().add(btn3);
-
-		btn4 = new JButton("4");
-		btn4.setBounds(49, 320, 56, 59);
-		frame.getContentPane().add(btn4);
-
-		btn5 = new JButton("5");
-		btn5.setBounds(115, 320, 56, 59);
-		frame.getContentPane().add(btn5);
-
-		btn6 = new JButton("6");
-		btn6.setBounds(181, 320, 56, 59);
-		frame.getContentPane().add(btn6);
-
-		btn7 = new JButton("7");
-		btn7.setBounds(49, 411, 56, 59);
-		frame.getContentPane().add(btn7);
-
-		btn8 = new JButton("8");
-		btn8.setBounds(115, 411, 56, 59);
-		frame.getContentPane().add(btn8);
-
-		btn9 = new JButton("9");
-		btn9.setBounds(181, 411, 56, 59);
-		frame.getContentPane().add(btn9);
-
-		btn0 = new JButton("0");
-		btn0.setBounds(49, 500, 56, 59);
-		frame.getContentPane().add(btn0);
-
-		btnSum = new JButton("+");
-		btnSum.setBounds(384, 231, 56, 59);
-		frame.getContentPane().add(btnSum);
-
-		btnSubtract = new JButton("-");
-		btnSubtract.setBounds(384, 320, 56, 59);
-		frame.getContentPane().add(btnSubtract);
-
-		btnMultiply = new JButton("*");
-		btnMultiply.setBounds(384, 411, 56, 59);
-		frame.getContentPane().add(btnMultiply);
-
-		btnDivide = new JButton("/");
-		btnDivide.setBounds(384, 500, 56, 59);
-		frame.getContentPane().add(btnDivide);
-
-		btnCalculate = new JButton("=");
-		btnCalculate.setBounds(115, 500, 122, 59);
-		frame.getContentPane().add(btnCalculate);
-
-		btnIsEven = new JButton("even?");
-		btnIsEven.setBounds(115, 141, 89, 59);
-		frame.getContentPane().add(btnIsEven);
-
-		btnC = new JButton("C");
-		btnC.setBounds(49, 141, 56, 59);
-		frame.getContentPane().add(btnC);
+		loadView();
 
 		// LOGIC: 0-9 NUMBER BUTTONS
 
@@ -254,6 +173,89 @@ public class BasicCalculator {
 				operationIsDefined = false;
 			}
 		});
+	}
+
+	private void loadView() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 522, 622);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.getContentPane().setLayout(null);
+
+		textField = new JTextField();
+		textField.setText("");
+		textField.setEnabled(false);
+		textField.setBounds(49, 30, 391, 70);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		btn1 = new JButton("1");
+		btn1.setBounds(49, 231, 56, 59);
+		frame.getContentPane().add(btn1);
+
+		btn2 = new JButton("2");
+		btn2.setBounds(115, 231, 56, 59);
+		frame.getContentPane().add(btn2);
+
+		btn3 = new JButton("3");
+		btn3.setBounds(181, 231, 56, 59);
+		frame.getContentPane().add(btn3);
+
+		btn4 = new JButton("4");
+		btn4.setBounds(49, 320, 56, 59);
+		frame.getContentPane().add(btn4);
+
+		btn5 = new JButton("5");
+		btn5.setBounds(115, 320, 56, 59);
+		frame.getContentPane().add(btn5);
+
+		btn6 = new JButton("6");
+		btn6.setBounds(181, 320, 56, 59);
+		frame.getContentPane().add(btn6);
+
+		btn7 = new JButton("7");
+		btn7.setBounds(49, 411, 56, 59);
+		frame.getContentPane().add(btn7);
+
+		btn8 = new JButton("8");
+		btn8.setBounds(115, 411, 56, 59);
+		frame.getContentPane().add(btn8);
+
+		btn9 = new JButton("9");
+		btn9.setBounds(181, 411, 56, 59);
+		frame.getContentPane().add(btn9);
+
+		btn0 = new JButton("0");
+		btn0.setBounds(49, 500, 56, 59);
+		frame.getContentPane().add(btn0);
+
+		btnSum = new JButton("+");
+		btnSum.setBounds(384, 231, 56, 59);
+		frame.getContentPane().add(btnSum);
+
+		btnSubtract = new JButton("-");
+		btnSubtract.setBounds(384, 320, 56, 59);
+		frame.getContentPane().add(btnSubtract);
+
+		btnMultiply = new JButton("*");
+		btnMultiply.setBounds(384, 411, 56, 59);
+		frame.getContentPane().add(btnMultiply);
+
+		btnDivide = new JButton("/");
+		btnDivide.setBounds(384, 500, 56, 59);
+		frame.getContentPane().add(btnDivide);
+
+		btnCalculate = new JButton("=");
+		btnCalculate.setBounds(115, 500, 122, 59);
+		frame.getContentPane().add(btnCalculate);
+
+		btnIsEven = new JButton("even?");
+		btnIsEven.setBounds(115, 141, 89, 59);
+		frame.getContentPane().add(btnIsEven);
+
+		btnC = new JButton("C");
+		btnC.setBounds(49, 141, 56, 59);
+		frame.getContentPane().add(btnC);
 	}
 
 	private void setActionListenerToJButtonNumber(JButton jButton) {
