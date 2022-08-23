0fef81e9dd644e5bff7b68949a368a39b92f65a8
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-08-23 19:36:45 +0200

    create calculator logic

---
+++ src/model/CalculatorLogic.java
@@ -0,0 +1,81 @@
+package model;
+
+public class CalculatorLogic {
+
+	private String operand;
+	private String operator;
+	private double computation;
+
+	public CalculatorLogic() {
+		reset();
+	}
+
+	public void reset() {
+		this.operand = "";
+		this.operator = null;
+		this.computation = 0;
+	}
+
+	public String getCurrentOperand() {
+		return this.operand;
+	}
+
+	public double getComputation() {
+		return this.computation;
+	}
+
+	public void assignOperand(String operand) {
+		if (this.operand.charAt(this.operand.length() - 1) == '.' && operand.equals(".")) {
+			return;
+		}
+		if (this.computation == 0 || operand.equals("")) {
+			this.operand = operand;
+		} else {
+			this.operand += operand;
+		}
+		if (this.operator == null) {
+			this.computation = Double.parseDouble(this.operand);
+		}
+	}
+
+	public void setOperator(String operator) {
+		if (this.operand.equals("") && this.computation == 0) {
+			return;
+		}
+		if (this.operator != null) {
+			this.compute();
+		} else {
+			this.operand = "";
+		}
+		this.operator = operator;
+	}
+
+	public void compute() {
+		if (this.operand.equals(" ")) {
+			return;
+		}
+		switch (this.operator) {
+		case "+":
+			this.computation += Double.parseDouble(this.operand);
+			break;
+		case "-":
+			this.computation -= Double.parseDouble(this.operand);
+			break;
+		case "*":
+			this.computation *= Double.parseDouble(this.operand);
+			break;
+		case "/":
+			this.computation /= Double.parseDouble(this.operand);
+			break;
+		default:
+			return;
+		}
+		this.operand = "";
+		this.operator = null;
+	}
+
+	public boolean isEven() {
+		return Double.parseDouble(this.operand) % 2 == 0;
+	}
+
+}
